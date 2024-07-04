51e2723d7c24754a9a79e46f3ad65dbf545b9900
meshach567 <arinzemeshach123@gmail.com>
2024-07-04 18:46:30 +0100

    commit tailwind config to ekene branch

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,11 +1,29 @@
-import { SignIn } from '@clerk/nextjs'
+// pages/sign-in/[[...sign-in]].tsx
+import { GetStaticPaths } from 'next';
+import { SignUp } from '@clerk/nextjs';
+
+// Define getStaticPaths to generate static paths
+export const getStaticPaths: GetStaticPaths = async () => {
+  // Define the possible paths
+  const paths = [{ params: { signIn: [] } }];
+
+  return {
+    paths,
+    fallback: 'blocking', // Set fallback to blocking to handle dynamic routes at runtime if needed
+  };
+};
+
+// If you need getStaticProps, you can add it here
+// export const getStaticProps: GetStaticProps = async () => {
+//   return { props: {} };
+// };
 
 const Page = () => {
   return (
     <div className="flex-center glassmorphism-auth h-screen w-full">
-      <SignIn />
+      <SignUp />
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
